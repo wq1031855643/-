e232597ab49a972788c5370a5cb97df58ec9612d
18307053705 <2596523458@qq.com>
2019-06-26 21:13:32 +0800

    版本1.15

--- js/f_library.js
+++ js/f_library.js
@@ -1,3 +1,5 @@
+
+//请求ajax函数
 function  aja(arr,code){
     $.ajax({
         url:"data/city.json",
@@ -10,6 +12,7 @@ function  aja(arr,code){
     })
 }
 
+//单击地区选择时，调用请求ajax函数，并获得相应数据
 function forArr(jsonArr){
     var str = "";
     for(var i=0,len = jsonArr.length ; i < len ;i++){
@@ -17,6 +20,7 @@ function forArr(jsonArr){
     }
     $(".f_cityList ul").html(str);
 }
+//鼠标移动到省会时，调用请求ajax函数，并获得相应数据
 function forArr2(jsonArr,code){
     var str = "";
     for(var i=0,len = jsonArr.length ; i < len ;i++){
@@ -32,4 +36,29 @@ function forArr2(jsonArr,code){
     $(".f_cityOist ol").html(str);
 }
 
+//单击地区选择，调用forArr函数
+$(".f_sel span").click(function(){
+    aja(forArr);
+    $(this).addClass("f_showCity bg")
+})
+//单击地区选择，调用forArr2函数
+$(".f_cityList ul").on("mouseover","li",function(){
+    var code = $(this).attr("code");
+    aja(forArr2,code);
+})
 
+//模拟滚动条
+$(".f_sor p").mousedown("click",function(e){
+    this.y = e.offsetY;
+    _this = this;
+    $(".f_sor").bind("mousemove",function(e){        
+        var lTop = e.offsetY - _this.y;
+        lTop = lTop < 0 ? 0 : lTop;
+        lTop = lTop > 250 ? 250 : lTop;
+        $(_this).css("top",lTop);
+        return false;
+    })
+    $(".f_cityList ul").mouseup(function(){
+       $(".f_sor").unbind("mousemove");
+    })
+})
