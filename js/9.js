a24c2fbb730bef5037273f10c7c78b89df2b6adb
xiangweng <1328184563@qq.com>
2019-06-27 20:49:19 +0800

    xw

---
+++ js/9.js
@@ -0,0 +1,22 @@
+$('.xw_nav_left').on('click','a',function(){
+    $('.xw_nav_left').children().children().removeClass('xw_js-left');
+    $(this).addClass('xw_js-left');
+    if($(this).text()=='APP'||$(this).text()=='资讯'){
+        $('#xw_wrap').attr('class','xw_wrap-b');
+        $('#xw_header').attr('class','xw_header-b');
+        $('#xw_logo').attr('class','xw_logo-b');
+        $('#xw_nav_left').addClass('xw_nav_left-b');
+        $('#xw_enter').addClass('xw_enter-b');
+        $('#xw_sign').addClass('xw_sign-b');
+        $('#xw_nav_right').addClass('xw_nav_right-b');
+    }else{
+        $('#xw_wrap').attr('class','xw_wrap');
+        $('#xw_header').attr('class','xw_header');
+        $('#xw_logo').attr('class','xw_logo');
+        $('#xw_nav_left').removeClass('xw_nav_left-b');
+        $('#xw_enter').removeClass('xw_enter-b');
+        $('#xw_sign').removeClass('xw_sign-b');
+        $('#xw_nav_right').removeClass('xw_nav_right-b');
+
+    }
+})
