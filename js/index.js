85e3ed11c27641a25043e3e277637f28e177d082
wq1031855643 <1031855643@qq.com>
2019-06-29 17:56:58 +0800

    update

--- js/index.js
+++ js/index.js
@@ -10,6 +10,8 @@ window.onscroll = function(){
 	fixedTop(l_seach_box,300);//吸顶效果
 }
 
+
+
 window.onload = function() {
 	axios.all([getPosition(), getPosition2()])
 		.then(axios.spread(function(response, perms) {
@@ -154,5 +156,23 @@ tree3.onclick = function(eve) {
 	var target = e.target || e.srcElement;
 	if (target.tagName == "LI") {
 		(document.getElementById("zhi")).innerHTML = target.innerHTML;
+	}
+}
+
+//吸顶效果函数
+function fixedTop(dom,height){
+	//获取浏览器滚走的距离 
+	var sTop = document.documentElement.scrollTop || document.body.scrollTop;
+	//与168比较
+	if(sTop > height){
+		//当浏览器滚动到168的距离时让导航nav固定定位，top为0
+		dom.style.position = "fixed";
+		dom.style.top = 0;
+		dom.style.backgroundColor = "#fff";
+		dom.style.width = "100%";
+	}else{
+		//否则 定位回到初始状态
+		dom.style.position = "static";
+		dom.style.backgroundColor = "#f6f6f8";
 	}
 }
