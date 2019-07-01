65de0c958bf606c87f54430bc580b40664f17dab
wq1031855643 <1031855643@qq.com>
2019-07-01 16:39:31 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
-var position = document.getElementsByClassName("l_position_sel")[0];
-var tree1 = document.getElementsByClassName("tree-1")[0];
-var tree2 = document.getElementsByClassName("tree-2")[0];
-var tree3 = document.getElementsByClassName("tree-3")[0];
-var job_menu = document.getElementsByClassName("job_menu")[0];
+var tree1 = document.querySelector(".tree-1");
+var tree2 = document.querySelector(".tree-2");
+var tree3 = document.querySelector(".tree-3");
 var list = ""; //职位数据
-var l_seach_box = document.querySelector(".l_seach_box ");
 
 window.onscroll = function(){
+	var l_seach_box = document.querySelector(".l_seach_box");
 	fixedTop(l_seach_box,300);//吸顶效果
 }
-
-
 
 window.onload = function() {
 	axios.all([getPosition(), getPosition2()])
@@ -18,19 +14,41 @@ window.onload = function() {
 			// console.log(response);
 			// console.log(perms.data);
 			if (response.status == 200) {
+				var job_menu = document.querySelector(".job_menu");
 				list = response.data.zpData; //第一级职位数据
 				var position2 = perms.data;
 				var str = "";
 				for (var i = 0; i < position2.length; i++) {
 					if ((position2[i].pList).length == 3) {
-						str = "<ul><li><i></i><b>" + position2[i].p + "</b><a href='#'>" + position2[i].pList[0] + "</a><a href='#'>" + position2[i].pList[1] + "</a><a href='#'>" + position2[i].pList[2] + "</a></li></ul>"
+						str = "<ul id='"+ position2[i].code +"'><li><i></i><b>" + position2[i].p + "</b><a href='#'>" + position2[i].pList[0] + "</a><a href='#'>" + position2[i].pList[1] + "</a><a href='#'>" + position2[i].pList[2] + "</a></li><div class='menu_sub'></div></ul>"
 					} else if ((position2[i].pList).length == 2) {
-						str = "<ul><li><i></i><b>" + position2[i].p + "</b><a href='#'>" + position2[i].pList[0] + "</a><a href='#'>" + position2[i].pList[1] + "</a></li></ul>"
+						str = "<ul id='"+ position2[i].code +"'><li><i></i><b>" + position2[i].p + "</b><a href='#'>" + position2[i].pList[0] + "</a><a href='#'>" + position2[i].pList[1] + "</a></li><div class='menu_sub'></div></ul>"
 					} else {
-						str = "<ul><li><i></i><b>" + position2[i].p + "</b><a href='#'>" + position2[i].pList[0] + "</a></li></ul>"
+						str = "<ul id='"+ position2[i].code +"'><li><i></i><b>" + position2[i].p + "</b><a href='#'>" + position2[i].pList[0] + "</a></li><div class='menu_sub'></div></ul>"
 					}
 					job_menu.innerHTML += str;
 				}
+				
+				$(".job_menu ul").on("mouseover",function(){
+					$(this).addClass("cur");
+					$(this).find("a").css("color","#fff");
+					$(this).find("b").css("color","#fff");
+					if ($(this).find(".menu_sub").children().length > 1) {
+						return;
+					}else{
+						$(this).find(".menu_sub").toggle();
+						showMemuSub($(this).attr("id"),$(this));
+					}
+					return false;
+				})
+				$(".job_menu ul").on("mouseout",function(){
+					$(this).find(".menu_sub").empty();
+					$(this).find(".menu_sub").toggle();
+					$(this).removeClass("cur");
+					$(this).find("a").css("color","#61687c");
+					$(this).find("b").css("color","#414a60");
+					return false;
+				})
 			}
 		}))
 		.catch(function(error) {
@@ -47,26 +65,20 @@ function getPosition2() {
 	return axios.get('data/position2.json');
 }
 
-job_menu.onmouseover = function(eve) {
-	var e = eve || event;
-	var target = e.target || e.srcElement;
-	if (target.tagName == "UL") {
-		target.className = "cur";
-	}
-}
-
-position.onclick = function(eve) {
+$(".l_position_sel").on("click",function(eve){
+	var li = "";
+	$(".l_select_tree").css("height","250px");
 	var e = eve || event;
 	if ((tree1.childNodes).length > 1) {
 		tree1.style.display = "block";
 	} else {
 		for (var i = 0; i < list.length; i++) {
-			var li = "<li id='" + list[i].code + "' data-i='" + i + "'>" + list[i].name + "</li>";
+			li = "<li id='" + list[i].code + "' data-i='" + i + "'>" + list[i].name + "</li>";
 			tree1.innerHTML += li;
 		}
 		tree1.style.display = "block";
 	}
-
+	
 	//阻止事件冒泡
 	if (e && e.stopPropagation) {
 		// this code is for Mozilla and Opera
@@ -75,13 +87,14 @@ position.onclick = function(eve) {
 		// this code is for IE
 		window.event.cancelBubble = true;
 	}
-}
+})
 
 
 document.onclick = function() {
 	tree1.style.display = "none";
 	tree2.style.display = "none";
 	tree3.style.display = "none";
+	(document.querySelector(".l_select_tree")).style.height = 0;
 }
 
 //第一级职位ul事件委托
@@ -100,13 +113,14 @@ tree1.onmouseover = function(eve) {
 }
 
 function showTree2(index) {
+	var li = "";
 	if ((tree2.childNodes).length > 0) {
 		tree2.innerHTML = "";
 	}
 	tree2.style.display = "block";
 	var subLevelModelList = list[index].subLevelModelList;
 	for (var i = 0; i < subLevelModelList.length; i++) {
-		var li = "<li id='" + subLevelModelList[i].code + "' data-l='" + index + "' data-i='" + i + "'>" + subLevelModelList[
+		li = "<li id='" + subLevelModelList[i].code + "' data-l='" + index + "' data-i='" + i + "'>" + subLevelModelList[
 			i].name + "</li>";
 		tree2.innerHTML += li;
 	}
@@ -127,13 +141,14 @@ tree2.onmouseover = function(eve) {
 }
 
 function showTree3(index1, index2) {
+	var li = "";
 	if ((tree3.childNodes).length > 0) {
 		tree3.innerHTML = "";
 	}
 	tree3.style.display = "block";
 	var subLevelModelList = list[index2].subLevelModelList[index1].subLevelModelList;
 	for (var i = 0; i < subLevelModelList.length; i++) {
-		var li = "<li id='" + subLevelModelList[i].code + "'>" + subLevelModelList[i].name + "</li>";
+		li = "<li id='" + subLevelModelList[i].code + "'>" + subLevelModelList[i].name + "</li>";
 		tree3.innerHTML += li;
 	}
 }
@@ -174,5 +189,30 @@ function fixedTop(dom,height){
 		//否则 定位回到初始状态
 		dom.style.position = "static";
 		dom.style.backgroundColor = "#f6f6f8";
+	}
+}
+
+
+function showMemuSub(id,ul){
+	var menu_sub = ul.find(".menu_sub");
+	var listchild = "";
+	var listchild_child = "";
+	var str = "";
+	var titleStr = "";
+	for (var i = 0; i < list.length; i++) {
+		if(list[i].code == id){
+			titleStr = '<p class="menu_title">'+ list[i].name +'</p>';
+			menu_sub.append(titleStr);
+			listchild = list[i].subLevelModelList;
+			for(var j = 0;j < listchild.length; j++){
+				listchild_child = listchild[j].subLevelModelList;
+				str = '<li><h4>'+ listchild[j].name +'</h4><div id="text_list'+ j +'"></div></li>';
+				menu_sub.append(str);
+				for(var k = 0;k < listchild_child.length;k++){
+					$("#text_list"+ j +"").append('<a href="#">'+ listchild_child[k].name +'</a>');
+				}
+			}
+		}
+		
 	}
 }
