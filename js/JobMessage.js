668ae014dff785a17b2923e8b8206150b8e982af
13667102501 <770113401@qq.com>
2019-07-02 21:59:10 +0800

    zhu7.2 3.0

--- js/JobMessage.js
+++ js/JobMessage.js
@@ -1,6 +1,32 @@
-define(['jquery'],function(){
-  
-        console.log($)
- 
+define(['jquery'], function () {
+
+        $.ajax({
+                type: 'get',
+                url: 'http://localhost/project/data/job.json',
+                cache: false,
+                dataType: 'json',
+                success: function (json) {
+                        $('.job_status').html(json.title)
+                        $('.z_name h1').html(json.job)
+                        $('.salary').html(json.salary)
+                        var str=''
+                        for(var i=0;i<json.welfare.length;i++){
+                                str+='<span>'+json.welfare[i]+'</span>'
+                        }
+                        $('.job_tag').html(str)
+                        $('.z_more').show()
+                        $('.link_more').hover(function(){
+                                $('.z_tagAll').show().html(str)
+                        },function(){
+                                $('.z_tagAll').hide()
+                        })
+                }
+        })
+        $(".z_btn").bind('click',function(){
+                localStorage.setItem('status','reg')
+        })
+
+
+
 })
 
