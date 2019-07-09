804c3a16b56e94f9b14ada4149c4368072d49f97
wq1031855643 <1031855643@qq.com>
2019-07-09 19:38:59 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ var load = require("gulp-load-plugins")();
 // var gutil = require("gulp-util");
 
 gulp.task('minifyJS', function () {
-    gulp.src('./js/app.js') // 要压缩的js文件
+    gulp.src('./js/headMod.js') // 要压缩的js文件
     .pipe(load.uglify()) //使用uglify进行压缩
     .pipe(gulp.dest('./dist/js/'));//写入js文件夹
 });
 
 gulp.task('minifyCss', function () {
-    gulp.src('./css/app.css') // 要压缩的css文件
+    gulp.src('./css/*.css') // 要压缩的css文件
     .pipe(load.minifyCss()) //压缩css
     .pipe(gulp.dest('./dist/css'));//写入dist文件夹
 });
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const server = express();
 
-server.use('/dist/css', express.static('./dist/css'));
-server.use('/dist/js', express.static('./dist/js'));
-server.use('/dist/images', express.static('./dist/images'));
-server.use('/dist/data', express.static('./dist/data'));
+server.use('/css', express.static('./dist/css'));
+server.use('/js', express.static('./dist/js'));
+server.use('/images', express.static('./dist/images'));
+server.use('/data', express.static('./dist/data'));
 
 server.get('/', (req, res)=>{
 	res.sendFile(__dirname + '/dist/index.html');
@@ -13,6 +13,34 @@ server.get('/', (req, res)=>{
 
 server.get('/posi', (req, res)=>{
 	res.sendFile(__dirname + '/dist/posi.html');
+})
+
+server.get('/JobMessage', (req, res)=>{
+	res.sendFile(__dirname + '/dist/JobMessage.html');
+})
+
+server.get('/comdetails', (req, res)=>{
+	res.sendFile(__dirname + '/dist/comdetails.html');
+})
+
+server.get('/company', (req, res)=>{
+	res.sendFile(__dirname + '/dist/company.html');
+})
+
+server.get('/app', (req, res)=>{
+	res.sendFile(__dirname + '/dist/app.html');
+})
+
+server.get('/message', (req, res)=>{
+	res.sendFile(__dirname + '/dist/message.html');
+})
+
+server.get('/login', (req, res)=>{
+	res.sendFile(__dirname + '/dist/login.html');
+})
+
+server.get('/resume', (req, res)=>{
+	res.sendFile(__dirname + '/dist/resume.html');
 })
 //启动服务
 server.listen(80,(error)=>{
